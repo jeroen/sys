b7b0f4a6a0c52000acda17d98f71cc4967f6c81d
Jeroen Ooms <jeroenooms@gmail.com>
2017-01-09 15:32:13 +0100

    Wording stuff

--- R/exec.R
+++ R/exec.R
@@ -1,18 +1,22 @@
 #' Execute a program
 #'
-#' Flexible replacements for [base::system2].
+#' Flexible and robust replacements for [base::system2].
 #'
-#' The `exec_with_wait` function runs a program and returns the status code
-#' of the child program. Returns if the process completes (either with success
-#' or error) but raises an error if the process is terminated by a SIGNAL.
+#' The `exec_with_wait` function runs a program and waits for it to complete.
+#' If the process completes (either with success or error) it returns the status
+#' code, but an error is raised when the process is terminated by a SIGNAL.
+#' The R user can interrupt the program by sending SIGINT (press ESC or CTRL+C)
+#' in which case the process tree is properly terminated.
 #'
 #' The `exec_with_pid` function returns immediately with the PID
 #' of the child program. In this case the state of the child is unknown.
-#' You should kill it manually with [tools::pskill].
+#' You should kill it manually with [tools::pskill]. This is useful for running
+#' a server daemon or background process.
 #'
 #' @export
 #' @rdname exec
-#' @param cmd the command to run
+#' @param cmd the command to run. Eiter a full path or the name of a program
+#' which exists in the `PATH`.
 #' @param args character vector of arguments to pass
 #' @useDynLib sys C_run_with_pid
 exec_with_wait <- function(cmd, args = NULL){
