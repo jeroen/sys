c40fdc2d9d212374077f614a68844c65dc2a6f73
Jeroen Ooms <jeroenooms@gmail.com>
2017-01-16 17:20:10 +0100

    Add test for stdout

---
+++ tests/testthat/test-stdout.R
@@ -0,0 +1,14 @@
+context("stdout and stderr")
+
+test_that("test output for std_out equals TRUE/FALSE", {
+  string <- "hello world!"
+  output1 <- capture_output(exec_wait('echo', string))
+  output2 <- capture_output(exec_wait('echo', string, std_out = FALSE))
+  expect_equal(output1, string)
+  expect_equal(output2, "")
+
+  output3 <- capture.output(res <- exec_wait('ping', 'asfdsafdsfasdfasdf'), type = 'message')
+  output4 <- capture.output(res <- exec_wait('ping', 'asfdsafdsfasdfasdf', std_err = FALSE), type = 'message')
+  expect_length(output3, 1)
+  expect_length(output4, 0)
+})
