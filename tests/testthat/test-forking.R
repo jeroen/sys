9b3fc194aec1e0000e9dbcb4cef4482271a2a81d
Jeroen Ooms <jeroenooms@gmail.com>
2017-02-12 21:23:10 +0100

    Add some unit tests for eval_fork()

---
+++ tests/testthat/test-forking.R
@@ -0,0 +1,31 @@
+context("eval_fork")
+
+test_that("eval_fork works", {
+  skip_on_os("windows")
+
+  # PID must be different
+  expect_true(Sys.getpid() != eval_fork(Sys.getpid()))
+
+  # State is inherited
+  n <- 10
+  x <- eval_fork(rnorm(n))
+  expect_identical(x, rnorm(n))
+
+  # Test cleanups
+  for(i in 1:300){
+    expect_equal(pi, eval_fork(pi))
+  }
+})
+
+test_that("eval_fork gives errors", {
+  skip_on_os("windows")
+
+  # Test regular errors
+  expect_error(eval_fork(stop("uhoh")), "uhoh")
+  expect_error(eval_fork(blablabla()), "could not find function")
+
+  # Test that proc dies properly
+  expect_error(eval_fork(tools::pskill(Sys.getpid())), "child process died")
+  expect_error(eval_fork(Sys.sleep(10), timeout = 2), "timeout")
+})
+
