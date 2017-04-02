938819c0bbd37a92c63cb5dfffcfd80e5c5c0922
Jeroen Ooms <jeroenooms@gmail.com>
2017-04-02 13:33:10 +0200

    Try making symbol invisible

--- src/fork.c
+++ src/fork.c
@@ -2,6 +2,7 @@
 #include <Rinterface.h>
 #include <Rembedded.h>
 #include <Rconfig.h>
+#include <R_ext/Visibility.h>
 #include <unistd.h>
 #include <signal.h>
 #include <string.h>
@@ -97,7 +98,7 @@ static void serialize_to_pipe(SEXP object, int results[2]){
   R_Serialize(object, &stream);
 }
 
-void prepare_fork(const char * tmpdir){
+void attribute_hidden prepare_fork(const char * tmpdir){
 #ifndef R_SYS_BUILD_CLEAN
   ptr_R_WriteConsole = NULL;
   ptr_R_WriteConsoleEx = write_out_ex;
