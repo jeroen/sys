a8733caef6fc0f3b01ec27083293ee2faffd7ce4
Jeroen Ooms <jeroenooms@gmail.com>
2017-04-14 20:25:01 +0200

    Refactor, fixes windows

--- src/util.c
+++ src/util.c
@@ -1,5 +1,7 @@
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
+#include <Rembedded.h>
+#include <string.h>
 
 void R_init_sys(DllInfo *info) {
   R_registerRoutines(info, NULL, NULL, NULL, NULL);
@@ -12,4 +14,15 @@ SEXP R_safe_build(){
 #else
   return ScalarLogical(FALSE);
 #endif
+}
+
+
+SEXP R_set_tempdir(SEXP path){
+  const char * tmpdir = CHAR(STRING_ELT(path, 0));
+#ifdef SYS_BUILD_SAFE
+  R_TempDir = strdup(tmpdir);
+#else
+  Rf_error("Cannot set tempdir(), sys has been built without SYS_BUILD_SAFE");
+#endif
+  return path;
 }
