dd525d18078c81b288bdf3446f0af7bf082595ce
Jeroen Ooms <jeroenooms@gmail.com>
2017-04-15 23:59:40 +0200

    Fix Windows

--- src/util.c
+++ src/util.c
@@ -46,9 +46,9 @@ SEXP R_set_tempdir(SEXP path){
   return path;
 }
 
+extern Rboolean R_Interactive;
 SEXP R_set_interactive(SEXP set){
 #ifdef SYS_BUILD_SAFE
-  extern Rboolean R_Interactive;
   R_Interactive = asLogical(set);
 #else
   Rf_error("Cannot set interactive(), sys has been built without SYS_BUILD_SAFE");
