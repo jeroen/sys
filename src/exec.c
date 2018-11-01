62e3c397efd790793b22e7e052848301aca1146e
Jeroen Ooms <jeroenooms@gmail.com>
2018-11-01 21:57:00 +0100

    Allow for std_in = NULL

--- src/exec.c
+++ src/exec.c
@@ -176,7 +176,7 @@ SEXP C_execute(SEXP command, SEXP args, SEXP outfun, SEXP errfun, SEXP wait, SEX
     //OSX: do NOT change pgid, so we receive signals from parent group
 
     // Set STDIN for fork (default is /dev/null)
-    set_input(CHAR(STRING_ELT(input, 0)));
+    set_input(IS_STRING(input) ? CHAR(STRING_ELT(input, 0)) : "/dev/null");
 
     //close all file descriptors before exit, otherwise they can segfault
     for (int i = 3; i < sysconf(_SC_OPEN_MAX); i++) {
