cb8ac00d740b5f28fe1adb5fadf6b690393e9e87
Jeroen Ooms <jeroenooms@gmail.com>
2018-11-05 14:31:47 +0100

    Add exec_r wrappers

---
+++ R/callr.R
@@ -0,0 +1,39 @@
+#' Execute R from R
+#'
+#' Convenience wrappers for [exec_wait] and [exec_internal] that shell out to
+#' R itself: `R.home("bin/R")`.
+#'
+#' This is a simple but robust way to invoke R commands in a separate process.
+#' Use the [callr](https://cran.r-project.org/web/packages/callr/index.html)
+#' package if you need more sophisticated control over (multiple) R process jobs.
+#'
+#' @export
+#' @rdname exec_r
+#' @name exec_r
+#' @inheritParams exec
+#' @param args command line arguments for R
+#' @param std_in a file to send to stdin, usually an R script (see examples).
+#' @examples # Hello world
+#' exec_r("--version")
+#'
+#' # Run some code
+#' exec_r(c('--vanilla', '-q', '-e', 'sessionInfo()'))
+#'
+#' # Run a script via stdin
+#' tmp <- tempfile()
+#' writeLines(c("x <- rnorm(100)", "mean(x)"), con = tmp)
+#' exec_r(std_in = tmp)
+exec_r <- function(args = '--vanilla', std_out = stdout(), std_err = stderr(), std_in = NULL){
+  exec_wait(rbin(), args = args, std_out = std_out, std_err = std_err, std_in = std_in)
+}
+
+#' @export
+#' @rdname exec_r
+exec_r_internal <- function(args = '---vanilla', std_in = NULL, error = TRUE){
+  exec_internal(rbin(), args = args, std_in = std_in, error = error)
+}
+
+rbin <- function(){
+  cmd <- ifelse(.Platform$OS.type == 'windows', 'Rterm', 'R')
+  file.path(R.home('bin'), cmd)
+}
