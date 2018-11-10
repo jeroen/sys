2d48a4e1eb17fb4ba63a65b9512667fa4d85f17f
Jeroen Ooms <jeroenooms@gmail.com>
2018-11-10 14:26:18 +0100

    Remove 'ps' dependency

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,9 +1,4 @@
 library(testthat)
 library(sys)
 
-if (ps::ps_is_supported()) {
-  reporter <- ps::CleanupReporter(testthat::CheckReporter)$new(proc_cleanup =  FALSE,  proc_fail = FALSE)
-  test_check("sys", reporter = reporter)
-} else {
-  test_check("sys")
-}
+test_check("sys")
