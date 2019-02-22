3fbc3d75b20f3f3865334713beff0926a70bdc54
Jeroen Ooms <jeroenooms@gmail.com>
2019-02-22 15:14:02 +0100

    More legacy wrappers

--- R/legacy.R
+++ R/legacy.R
@@ -1,9 +1,21 @@
 #' Legacy functions
 #'
-#' These functions have moved into the `unix` package.
+#' These functions have moved into the `unix` package. Please update
+#' your references.
 #'
 #' @export
-#' @param ... see `?eval_safe` in unix
+#' @rdname legacy
+#' @param ... see respective functions in the unix package
 eval_safe <- function(...){
   unix::eval_safe(...)
+}
+
+#' @export
+#' @rdname legacy
+aa_config <- function(...){
+  unix::aa_config(...)
+}
+
+set_tempdir <- function(...){
+  unix::set_tempdir(...)
 }
